89aabf5fbe7e58318612b6fda647c5366b9a0c76
Stephen K <stephenjk@optonline.net>
2019-10-29 18:01:00 -0400

    add attribute :new to images

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 	namespace :api do
 		namespace :v1 do
 		# denotes REST actions for images and butterflies
-        	resources :images, only: [:index, :create, :destroy, :update]
+        	resources :images, only: [:index, :new, :create, :destroy, :update]
         	resources :butterflies, only: [:index, :create, :destroy, :update]
     	end
 	end
