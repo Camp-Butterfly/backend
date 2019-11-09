c270f97de15ec5428fa6505784fa566f241ea374
Stephen K <stephenjk@optonline.net>
2019-11-08 22:12:36 -0500

    add request method and allow headers

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,9 @@ module Backend
     # the framework and any gems in your application.
 	  def set_cors
 	    headers['Access-Control-Allow-Origin'] = '*'
+	    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
 	    headers['Access-Control-Request-Method'] = '*'
+		headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
 	  end
   end
 end
