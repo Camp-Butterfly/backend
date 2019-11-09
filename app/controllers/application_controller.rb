8b1411a416f48974757088bfa7f38a79c95f88f4
Stephen K <stephenjk@optonline.net>
2019-11-08 22:30:14 -0500

    hope this fix works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,17 @@
 class ApplicationController < ActionController::Base
 	skip_before_action :verify_authenticity_token
 
-	def set_access_control_headers
+	def cors_set_access_control_headers
 	    headers['Access-Control-Allow-Origin'] = '*'
 	    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
 	    headers['Access-Control-Request-Method'] = '*'
 		headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-	  end
+	end
+	
+	def set_cors_header
+	    headers['Access-Control-Allow-Origin'] = '*'
+	    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+	    headers['Access-Control-Request-Method'] = '*'
+	    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Typee, Accept, Authorization'
+  	end
 end
