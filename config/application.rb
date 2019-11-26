ee5e7c48cc978fb476b1f2e114affd8c3af88ee5
Stephen K <stephenjk@optonline.net>
2019-11-26 17:26:05 -0500

    whitelist localhost:3000 for http: and https:

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ module Backend
     # the framework and any gems in your application.
 	config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins '*'
+        origins 'http://localhost:3000', 'https://localhost:3000'
         resource '*', headers: :any, methods: [:get, :post, :options]
       end
     end
