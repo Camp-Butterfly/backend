649f222a013403eab97415ac18b4e785052e95ce
Stephen K <stephenjk@optonline.net>
2019-11-26 17:23:45 -0500

    add logout, login, current user methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,39 @@
 class ApplicationController < ActionController::Base
 	skip_before_action :verify_authenticity_token
 
+	helper_method :login!, :logged_in?, :current_user, :authorized_user?, :logout!
+
+	def login!
+		session[:user_id] = @user.id
+	end
+
+	def logged_in?
+		!!session[:user_id]
+	end
+
+	def current_user
+		@current_user ||= User.find(session[:user_id]) if session[:user_id]
+	end
+
+	def authorized_user?
+		@user == current_user
+	end
+
+	def logout!
+		session.clear
+	end
+
 	def cors_set_access_control_headers
-	    headers['Access-Control-Allow-Origin'] = '*'
+	    headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'
 	    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
-	    headers['Access-Control-Request-Method'] = '*'
+	    headers['Access-Control-Request-Method'] = 'http://localhost:3000'
 		headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
 	end
 	
 	def set_cors_header
-	    headers['Access-Control-Allow-Origin'] = '*'
+	    headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'
 	    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
-	    headers['Access-Control-Request-Method'] = '*'
+	    headers['Access-Control-Request-Method'] = 'http://localhost:3000'
 	    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Typee, Accept, Authorization'
   	end
 end
