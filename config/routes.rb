6860f8189ce72647c8309027d23c8a5593dd49fe
Stephen K <stephenjk@optonline.net>
2019-11-26 11:08:38 -0500

    edit documentation

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,12 @@ Rails.application.routes.draw do
   post "/graphql", to: "graphql#execute"
 	namespace :api do
 		namespace :v1 do
-		# denotes REST actions for images and butterflies
-        	resources :images, only: [:index, :new, :create, :destroy, :update]
+		# defines REST actions for images and butterflies
+    # =>  GET, index
+    # =>  POST, create
+    # =>  DELETE, destroy
+    # =>  PUT, update
+        	resources :images, only: [:index, :create, :destroy, :update]
         	resources :butterflies, only: [:index, :create, :destroy, :update]
     	end
 	end
