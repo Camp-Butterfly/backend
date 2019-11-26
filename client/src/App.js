f9a309b1692938b828202a8bd94f397b1e5d97ae
Stephen K <stephenjk@optonline.net>
2019-11-26 17:24:43 -0500

    add handleLogin, handleLogout functions along with exact paths within app

--- client/src/App.js
+++ client/src/App.js
@@ -1,50 +1,99 @@
 import React, {Component} from 'react';
 import './App.css';
-import SearchForm from './forms/SearchForm.js';
+import Home from './components/Home.js';
+import Login from './registrations/Login.js';
+import Signup from './registrations/Signup.js';
 import ButterflyForm from './forms/ButterflyForm.js';
 import ImageForm from './forms/ImageForm.js'
 import {
-  BrowserRouter as Router,
+  BrowserRouter,
   Switch,
   Route,
-  Link
 } from "react-router-dom";
+import axios from 'axios';
 
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      isLoggedIn: false,
+      user: {}
+    };
+  }
+
+  componentDidMount() {
+    this.loginStatus()
+  }
+
+  loginStatus = () => {
+    axios.get('http://localhost:3001/logged_in', { withCredentials: true })
+    .then(response => {
+      if(response.data.logged_in) {
+        this.handleLogin(response)
+      } else {
+      this.handleLogout()
+    }
+  })
+    .catch(error => console.log('api errors:',error))
+  }
+
+  handleLogin = (data) => {
+    this.setState({
+      isLoggedIn: true,
+      user: data.user
+    })
+  }
+
+  handleLogout = () => {
+    this.setState({
+      isLoggedIn: false,
+      user: {}
+    })
+  }
+
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
             <h2>Camp Butterfly</h2>
         </header>
-          <Router>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/add">Add</Link>
-              </li>
-              <li>
-                <Link to="/gallery">Gallery</Link>
-              </li>
-            </ul>
+          <BrowserRouter>
             <Switch>
           {/* Home Page */}
-              <Route exact path="/">
-                <SearchForm />
-              </Route>
-          {/* Add Butterfly */}
-              <Route path="/add">
-                <ButterflyForm />
-              </Route>
-          {/* Add Image */}
-              <Route exact path="/gallery">
-                <ImageForm/>
-              </Route>
+              <Route
+                exact path='/'
+                render={props => (
+                  <Home {...props} handleLogout={this.handleLogout} loggedInStatus={this.state.isLoggedIn}/>
+                  )}
+                />
+              <Route
+                exact path='/login'
+                render={props => (
+                  <Login {...props} handleLogin={this.handleLogin} loggedInStatus={this.state.isLoggedIn}/>
+                  )}
+                />
+              <Route
+                exact path='/signup'
+                render={props => (
+                  <Signup {...props} handleLogin={this.handleLogin} loggedInStatus={this.state.isLoggedIn}/>
+                  )}
+              />
+              <Route 
+                exact path='/add'
+                render={props => (
+                  <ButterflyForm {...props} handleLogin={this.handleLogin} loggedInStatus={this.state.isLoggedIn} />
+                  )}
+                />
+              <Route 
+                exact path="/gallery"
+                render={props => (
+                  <ImageForm {...props} handleLogin={this.handleLogin} loggedInStatus={this.state.isLoggedIn} />
+                  )}
+              />
             </Switch>
-          </Router>
+          </BrowserRouter>
       </div>
     );
   }
