84938f812468304f8b1d269fd080ac22636b76bf
Stephen K <stephenjk@optonline.net>
2019-12-10 14:59:44 -0500

    partial implementation of grpc script

--- app/controllers/api/v1/images_controller.rb
+++ app/controllers/api/v1/images_controller.rb
@@ -13,29 +13,79 @@ class Api::V1::ImagesController < Api::V1::BaseController
     
     # pass this as argument to python function 
     # image_params[:image_content]
-    obj = image_params[:longitude]
+#02, 03    
+obj = image_params[:longitude]
 
     require 'rubypython'
-    RubyPython.start
-    #sample = RubyPython.import("test.sample")
-    # json = RubyPython.import("test.sample")
-    #value = sample.sample().rubify
-    #cPickle = RubyPython.import("cPickle")
-    obj = obj + "tomato"
-   
-    p (obj)
-    # response = "Testing RubyPython." + obj
-     p ("testing rubyp")
-    #json(obj)
-    RubyPython.stop
+    RubyPython.start  # start Python interpreter
 
-    #respond_with Image.find_by(longitude: params[:longitude])
-#image = Image.find_by(longitude: image_params[:longitude])     # 01; responds with image found
+#03 
+grpc = RubyPython.import("grpc")
+np  = RubyPython.import("numpy")
+#requests = RubyPython.import("requests") # this module is problematic
+#tf = RubyPython.import("tensorflow") # this module is problematic
+os  = RubyPython.import("os")
 
-    # returns json of result
-#respond_with image, json: image                                # 01
-   respond_with obj, json: obj
-    #respond_with cPickle, json: cPickle
+
+#from tensorflow_serving.apis import predict_pb2
+#from tensorflow_serving.apis import prediction_service_pb2_grpc
+#from tensorflow.keras.preprocessing import image
+
+#tf.compat.v1.app.flags.DEFINE_string('server', '173.255.119.154:80', 
+#        'PredictionService host:port')
+#tf.compat.v1.app.flags.DEFINE_string('image', '','path to image in JPEG format')
+
+#FLAGS = tf.compat.v1.app.flags.FLAGS
+
+#  #IMAGE_PATH = 'test_image_ringlet.JPEG'
+
+#if os.environ.get('https_proxy'):
+# del os.environ['https_proxy']
+#if os.environ.get('http_proxy'):
+# del os.environ['http_proxy']
+
+#def main(_):
+#  #Download image and convert to tensor
+#  img = image.load_img("test_image_ringlet.JPEG", target_size=(150,150))
+#  img_tensor = image.img_to_array(img)
+#  img_tensor = np.expand_dims(img_tensor, axis=0)
+#  data = img_tensor
+
+#  print(data)  
+
+#  channel = grpc.insecure_channel(FLAGS.server)
+#  stub = prediction_service_pb2_grpc.PredictionServiceStub(channel)
+#  #send request
+#  request = predict_pb2.PredictRequest()
+#  request.model_spec.name = 'test2'
+#  request.model_spec.signature_name = 'serving_default'
+#  request.inputs['input_image'].CopyFrom(
+#  tf.make_tensor_proto(data,shape=[1,150,150,3])
+#  )
+#  result = stub.Predict(request,10.0)
+#  print(result)
+
+#if __name__ == '__main__':
+#  tf.compat.v1.app.run()
+
+
+#obj = attempt.mod(obj).rubify
+
+#02
+#obj = obj + "tomato"  # appends tomato to longitude param
+#p (obj)               # prints concat in python interpreter
+
+    RubyPython.stop  # stop Python interpreter
+
+# 01; responds with json of found image
+#image = Image.find_by(longitude: image_params[:longitude])     
+
+# 01
+#respond_with image, json: image
+
+# 02, 03; responds with concat after python script, import modifier script
+respond_with obj, json: obj
+
   end
 
   def destroy
