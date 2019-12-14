ed3b7737fc5c25034f2a610153518a4dbe2e3b47
Stephen K <stephenjk@optonline.net>
2019-12-13 20:54:17 -0500

    parse highest probable type from tf model

--- app/controllers/api/v1/grpc_client_EDIT.py
+++ app/controllers/api/v1/grpc_client_EDIT.py
@@ -9,40 +9,42 @@ import os
 from tensorflow_serving.apis import predict_pb2
 from tensorflow_serving.apis import prediction_service_pb2_grpc
 from tensorflow.keras.preprocessing import image
-
-tf.compat.v1.app.flags.DEFINE_string('server', '173.255.119.154:80', 
-				'PredictionService host:port')
-tf.compat.v1.app.flags.DEFINE_string('image', '','path to image in JPEG format')
-
-FLAGS = tf.compat.v1.app.flags.FLAGS
-
-#IMAGE_PATH = 'test_image_ringlet.JPEG'
-
-if os.environ.get('https_proxy'):
- del os.environ['https_proxy']
-if os.environ.get('http_proxy'):
- del os.environ['http_proxy']
-
-def main(_):
+ 
+def main(channel):
   #Download image 
-  img = image.load_img("test_image_ringlet.JPEG", target_size=(150,150))
+  img = image.load_img("cabbage.jpg", target_size=(150,150))
   img_tensor = image.img_to_array(img)
   img_tensor = np.expand_dims(img_tensor, axis=0)
   data = img_tensor
 
   print(data)  
 
-  channel = grpc.insecure_channel(FLAGS.server)
+  options = [('grpc.min_reconnect_backoff_ms', 100)]
+  channel = grpc.insecure_channel('192.168.99.100:8500', options=options)
+  grpc.channel_ready_future(channel).result()
   stub = prediction_service_pb2_grpc.PredictionServiceStub(channel)
   #send request
   request = predict_pb2.PredictRequest()
-  request.model_spec.name = 'test2'
+  request.model_spec.name = 'testmodel'
   request.model_spec.signature_name = 'serving_default'
   request.inputs['input_image'].CopyFrom(
-	tf.make_tensor_proto(data,shape=[1,150,150,3])
+  tf.make_tensor_proto(data,shape=[1,150,150,3])
   )
-  result = stub.Predict(request,10.0)
+  result = stub.Predict(request, 100.0)
+  #channel.close()
   print(result)
+
+  print('\n')
+
+  print(floats)
+  floats = np.array(list(result.outputs['dense_1/Softmax:0'].float_val)) 
+  max_ = 0
+  for i in range(0,3):
+    if floats[i] > floats[max_]:
+      max_ = i
+  print(max_)
+
+  return channel
 
 if __name__ == '__main__':
   tf.compat.v1.app.run()
